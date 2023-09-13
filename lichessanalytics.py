2d8424093c28417f754734ab931ff28cdaf402b9
MohRok <144777102+MohRok@users.noreply.github.com>
2023-09-13 19:29:46 +0200

    Update lichessanalytics.py

--- lichessanalytics.py
+++ lichessanalytics.py
@@ -4,7 +4,7 @@ import plotly.graph_objs as go
 from dash.exceptions import PreventUpdate
 
 # Initialize the Dash app
-app = dash.Dash(meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
+app = Dash(meta_tags=[ {"name": "viewport", "content": "width=device-width, initial-scale=1"}])
 server = app.server
 # Define custom CSS styles
 custom_styles = {
