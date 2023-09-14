de3852e268117e983f6f2726a4f96f09d6f96a7e
MohRok <144777102+MohRok@users.noreply.github.com>
2023-09-15 01:36:44 +0200

    Update lichessanalytics.py

--- lichessanalytics.py
+++ lichessanalytics.py
@@ -43,11 +43,11 @@ app.layout = html.Div([
     html.Nav(
         children=[
             html.A("Home", href="/", style=custom_styles['nav_link']),
-            html.A("Option 1", href="/options/option1", style=custom_styles['nav_link']),
-            html.A("Option 2", href="/options/option2", style=custom_styles['nav_link']),
-            html.A("Option 3", href="/options/option3", style=custom_styles['nav_link']),
-            html.A("Option 4", href="/options/option4", style=custom_styles['nav_link']),
-            html.A("Option 5", href="/options/option5", style=custom_styles['nav_link']),
+            html.A("rating correlation", href="/options/option1", style=custom_styles['nav_link']),
+            html.A("rating history", href="/options/option2", style=custom_styles['nav_link']),
+            html.A(" Castling & Rooks", href="/options/option3", style=custom_styles['nav_link']),
+            html.A("Bishops vs Knights", href="/options/option4", style=custom_styles['nav_link']),
+            html.A("Openings", href="/options/option5", style=custom_styles['nav_link']),
             html.A("Option 6", href="/options/option6", style=custom_styles['nav_link']),
             html.A("Option 7", href="/options/option7", style=custom_styles['nav_link']),
             html.A("Option 8", href="/options/option8", style=custom_styles['nav_link']),
@@ -70,8 +70,8 @@ home_layout = html.Div([
 # Layout for Option 1 content
 option1_layout = html.Div([
     html.Div([
-        html.H1("Option 1", style={'margin-right': '20px'}),
-        html.P("This is the content for Option 1.", style={'font-size': '24px'}),
+        html.H1("rating correlation", style={'margin-right': '20px'}),
+        html.P("This is the content for rating correlation.", style={'font-size': '24px'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
@@ -79,8 +79,8 @@ option1_layout = html.Div([
 # Layout for Option 2 content
 option2_layout = html.Div([
     html.Div([
-        html.H1("Option 2", style={'margin-right': '20px'}),
-        html.P("This is the content for Option 2.", style={'font-size': '24px'}),
+        html.H1("rating history", style={'margin-right': '20px'}),
+        html.P("This is the content for rating history.", style={'font-size': '24px'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
@@ -88,16 +88,17 @@ option2_layout = html.Div([
 # Layout for Option 3 content
 option3_layout = html.Div([
     html.Div([
-        html.H1("Option 3", style={'margin-right': '20px'}),
-        html.P("This is the content for Option 3.", style={'font-size': '24px'}),
+        html.H1("Castling & Rooks", style={'margin-right': '20px'}),
+        html.P("This is the content for Castling & Rooks.", style={'font-size': '24px'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
-          'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
+          'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}),
+
 # Layout for Option 4 content
 option4_layout = html.Div([
     html.Div([
-        html.H1("Option 4", style={'margin-right': '20px'}),
-        html.P("This is the content for Option 4.", style={'font-size': '24px'}),
+        html.H1(" Bishops vs. Knights", style={'margin-right': '20px'}),
+        html.P("This is the content for Bishops vs. Knights.", style={'font-size': '24px'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
@@ -105,8 +106,8 @@ option4_layout = html.Div([
 # Layout for Option 5 content
 option5_layout = html.Div([
     html.Div([
-        html.H1("Option 5", style={'margin-right': '20px'}),
-        html.P("This is the content for Option 5.", style={'font-size': '24px'}),
+        html.H1("Openings", style={'margin-right': '20px'}),
+        html.P("This is the content for Openings.", style={'font-size': '24px'}),
     ], style={'display': 'flex', 'align-items': 'center'}),
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
@@ -162,54 +163,49 @@ def display_page(pathname):
     elif pathname == "/options/option8":
         return option8_layout
 
-    else:
+    elif pathname == '/':
         return home_layout
+    else:
+        raise PreventUpdate
 
 
 # Callback to update the graph content based on URL pathname
 @app.callback(
+
     Output('graph-content', 'children'),
     Input('url', 'pathname')
 )
 def update_graph(pathname):
-    if pathname == '/options/option1':
-        with open("/home/mohamad/Documents/UNI Kiel/DSProj/lichess_analytics/number_of_games_fig.json", "r") as file:
+    if pathname == '/':
+        return html.Div([
+            html.H3("About us ", style={'font-size': '24px'}),
+            html.P("Lichess.org is a free online chess website, that facilitates matches"
+                   "against random players all over the world."
+                   "Games are available in different Game-Modes, including Bullet, Blitz,"
+                   "Classical and Rapid."
+                   "The main difference between them is their duration."
+                   "For our analysis we have mainly focused on the game-mode Rapid,"
+                   "where players only have 10 minutes each to make all of their moves in"
+                   "the entire game."
+                   "A player loses if their king is checkmated or if they run out of time."
+                   "Our project is aimed at answering some questions regarding players’"
+                   "improvement and game strategies. For this purpose we analyzed"
+                   "approximately 32,000 high-rated rapid games, roughly 122,000"
+                   "amateur rapid matches and almost 700,000 high-rated blitz games. ")])
+
+    elif pathname == '/options/option1':
+        with open("number_of_games.json", "r") as file:
             number_of_games_fig = pio.from_json(file.read())
+        with open("number_of_puzzles.json", "r") as file:
+            number_of_puzzles = pio.from_json(file.read())
+        with open("puzzle_rating.json", "r") as file:
+            puzzle_rating = pio.from_json(file.read())
 
         return html.Div([
-            dcc.Graph(id='option1-chart', figure=number_of_games_fig),
-            dcc.Graph(id='option1-chart',
-                      figure={
-                          'data': [
-                              go.Scatter(
-                                  x=[1, 2, 3, 4, 5],
-                                  y=[10, 11, 12, 13, 14],
-                                  mode='markers',
-                                  name='Option 1 Data',
-                              ),
-                          ],
-                          'layout': {
-                              'title': 'Option 1 Chart',
-                          },
-                      }),
-            dcc.Graph(
-                id='option1-chart',
-                figure={
-                    'data': [
-                        go.Scatter(
-                            x=[1, 2, 3, 4, 5],
-                            y=[10, 11, 12, 13, 14],
-                            mode='markers',
-                            name='Option 1 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 1 Chart',
-                    },
-                }
+            dcc.Graph(id='option1-chart-1', figure=number_of_games_fig),
+            dcc.Graph(id='option1-chart-2', figure=number_of_puzzles),
+            dcc.Graph(id='option1-chart-3', figure=puzzle_rating)])
 
-            )
-        ])
     elif pathname == '/options/option2':
 
         return html.Div([
@@ -260,148 +256,49 @@ def update_graph(pathname):
             )
         ])
     elif pathname == '/options/option3':
-        return html.Div([
-            dcc.Graph(
-                id='option3-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 3 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 3 Chart',
-                    },
-                }),
-            dcc.Graph(
-                id='option3-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 3 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 3 Chart',
-                    },
-                }),
-            dcc.Graph(
-                id='option3-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 3 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 3 Chart',
-                    },
-                }
+        with open("1_a_castling_early_VIOLIN.json", "r") as file:
+            fig1_a = pio.from_json(file.read())
+        with open("2_a_rook_act_early_VIOLIN.json", "r") as file:
+            fig2_a = pio.from_json(file.read())
+        with open("3_a_castle_which_side_BAR.json", "r") as file:
+            fig3_a = pio.from_json(file.read())
 
-            )
-        ])
+        return html.Div([
+            html.H3("Does castling early help to win a match?"),
+            dcc.Graph(id='option3-chart-1', figure=fig1_a),
+            html.H3("Do the games in which the rooks are activated early result in a win?"),
+            dcc.Graph(id='option3-chart-2', figure=fig2_a),
+            html.H3("Castling on which side results in a win? How does castling affect the outcome of a game?"),
+            dcc.Graph(id='option3-chart-3', figure=fig2_a)])
 
     elif pathname == '/options/option4':
-        return html.Div([
-            dcc.Graph(
-                id='option4-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 4 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 4 Chart',
-                    },
-                }),
-            dcc.Graph(
-                id='option4-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 4 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 3 Chart',
-                    },
-                }),
-            dcc.Graph(
-                id='option4-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 4 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 4 Chart',
-                    },
-                }
+        with open("4_a_bishop_vs_knight_BAR.json", "r") as file:
+            fig4_a = pio.from_json(file.read())
 
-            )
+        return html.Div([
+            html.H3("Does having bishops or knights during an endgame result in a win?"),
+            dcc.Graph(id='option4-chart-1', figure=fig4_a),
         ])
+
     elif pathname == '/options/option5':
+        with open("5_a_opening_game_mode_MULT_PIE.json", "r") as file:
+            fig5_a = pio.from_json(file.read())
+        with open("6_a_opening_elo_HEATMAP.json", "r") as file:
+            fig6_a = pio.from_json(file.read())
+        with open("6_b_opening_elo_SCATTER_CAT.json", "r") as file:
+            fig6_b = pio.from_json(file.read())
+
         return html.Div([
-            dcc.Graph(
 
-                id='option5-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 5 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 5 Chart',
-                    },
-                }),
-            dcc.Graph(
+            html.H3("What openings are played by the groups of players belonging to different game modes?"),
+            dcc.Graph(id='option5-chart-1', figure=fig5_a),
+            html.H3("What openings are played by the groups of players belonging to different rating categories?"),
+            dcc.Graph(id='option5-chart-2', figure=fig6_a),
+            dcc.Graph(id='option5-chart-3', figure=fig6_b),
+            html.P("Visit this link for information about openings categorization."),
+            html.A('https://www.chessprogramming.org/ECO', href='https://www.chessprogramming.org/ECO',
+                   style=custom_styles['nav_link']),
 
-                id='option5-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 5 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 5 Chart',
-                    },
-                }),
-            dcc.Graph(
-
-                id='option5-chart',
-                figure={
-                    'data': [
-                        go.Pie(
-                            labels=['Category A', 'Category B', 'Category C'],
-                            values=[40, 30, 20],
-                            name='Option 5 Data',
-                        ),
-                    ],
-                    'layout': {
-                        'title': 'Option 5 Chart',
-                    },
-                }
-            )
         ])
     elif pathname == '/options/option6':
         return html.Div([
