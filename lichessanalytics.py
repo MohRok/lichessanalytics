5f5206f59ac42407b4eb20dee53d209fef835154
Mohamad Subhia <stu232381@mail.uni-kiel.de>
2023-09-18 14:48:39 +0200

    update Python

--- lichessanalytics.py
+++ lichessanalytics.py
@@ -1,5 +1,17 @@
+"""
+Lichess Data Science Project Web Application
+
+This Dash web application displays various data visualizations and insights related to Lichess.org, an online chess
+platform.
+
+Usage:
+1. Run this script to start the web application.
+2. Access the application by opening a web browser.
+
+"""
+# Import necessary libraries
+# import webbrowser  # Import the webbrowser module for opening the web application on localhost.
 from dash import Dash, dcc, html, Input, Output
-import plotly.graph_objs as go
 import plotly.io as pio
 from dash.exceptions import PreventUpdate
 
@@ -7,7 +19,7 @@ from dash.exceptions import PreventUpdate
 app = Dash(__name__)
 server = app.server
 
-# Define custom CSS styles
+# Define custom CSS styles for the application
 custom_styles = {
     'navbar': {
         'background-image': 'linear-gradient(to left, black, white)',
@@ -32,7 +44,7 @@ custom_styles = {
 
 }
 
-# Layout of the home page
+# Define the layout of the home page
 app.layout = html.Div([
     dcc.Location(id='url', refresh=False),
     html.Div(id='page-content'),
@@ -40,6 +52,7 @@ app.layout = html.Div([
         html.Img(src="/assets/lichesslogo.png", alt="foto", width="15%", lang="15%",
                  style={'position': 'absolute', 'top': '0px', 'right': '0px'}),
     ]),
+    # Navigation bar
     html.Nav(
         children=[
             html.A("Home", href="/", style=custom_styles['nav_link']),
@@ -58,7 +71,7 @@ app.layout = html.Div([
 
 ])
 
-# Layout of the home page content
+# Define the layout of the home page content
 home_layout = html.Div([
     html.Div([
         html.H1("Welcome to Data Science Project Lichess Page", style={'margin-right': '30%'}),
@@ -66,7 +79,7 @@ home_layout = html.Div([
     html.P("Team: Michel, Mohamad, Viktoria.", style={'font-size': '24px'}),
 ], style={'background-image': 'linear-gradient(to bottom, gray, white)', 'color': 'black', 'padding': '10px'})
 
-# Layout for Option 1 content
+# Layout for Option 0 content (Distributions on Lichess)
 option0_layout = html.Div([
     html.Div([
         html.H1("Distributions on Lichess", style={'margin-right': '20px'}),
@@ -74,7 +87,7 @@ option0_layout = html.Div([
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
 
-# Layout for Option 1 content
+# Layout for Option 1 content (Rating Correlation)
 option1_layout = html.Div([
     html.Div([
         html.H1("Rating Correlation", style={'margin-right': '20px'}),
@@ -82,7 +95,7 @@ option1_layout = html.Div([
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
 
-# Layout for Option 2 content
+# Layout for Option 2 content (Rating History)
 option2_layout = html.Div([
     html.Div([
         html.H1("Rating History", style={'margin-right': '20px'}),
@@ -90,7 +103,7 @@ option2_layout = html.Div([
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
 
-# Layout for Option 3 content
+# Layout for Option 3 content (Castling & Rooks)
 option3_layout = html.Div([
     html.Div([
         html.H1("Castling & Rooks", style={'margin-right': '20px'}),
@@ -98,7 +111,7 @@ option3_layout = html.Div([
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}),
 
-# Layout for Option 4 content
+# Layout for Option 4 content (Bishops vs Knights)
 option4_layout = html.Div([
     html.Div([
         html.H1(" Bishops vs. Knights", style={'margin-right': '20px'}),
@@ -106,7 +119,7 @@ option4_layout = html.Div([
 ], style={'background-image': 'linear-gradient(to Bottom, gray, white)', 'color': 'black', 'padding': '10px',
           'display': 'flex', 'justify-content': 'link', 'align-items': 'center'}, )
 
-# Layout for Option 5 content
+# Layout for Option 5 content (Openings)
 option5_layout = html.Div([
     html.Div([
         html.H1("Openings", style={'margin-right': '20px'}),
@@ -121,6 +134,17 @@ option5_layout = html.Div([
     Input("url", "pathname"),
 )
 def display_page(pathname):
+    """
+    This function determines which layout to display on the web page based on the URL pathname.
+
+    Parameters:
+        pathname (str): The URL pathname of the current web page.
+
+    Returns:
+        layout (dash.html.Div or dash.exceptions.PreventUpdate): The layout to be displayed, or PreventUpdate
+            if the pathname doesn't match any predefined options.
+    """
+
     if pathname == "/options/option0":
         return option0_layout
     elif pathname == "/options/option1":
@@ -146,10 +170,20 @@ def display_page(pathname):
     Input('url', 'pathname')
 )
 def update_graph(pathname):
+    """
+    This function updates and displays specific graph content on the web page based on the URL pathname.
+
+    Parameters:
+        pathname (str): The URL pathname of the current web page.
+
+    Returns:
+        graph_content (dash.html.Div): The graph content to be displayed on the web page.
+    """
     config = {'modeBarButtonsToRemove': ['autoscale2d', 'lasso2d', 'select2d'], 'displaylogo': False,
               'displayModeBar': True}
 
     if pathname == '/':
+        # Display information about the project on the home page
         return html.Div([
             html.H3("About our project", style={'font-size': '24px'}),
             html.P("Lichess.org is a free online chess website, that facilitates matches"
@@ -166,9 +200,10 @@ def update_graph(pathname):
                    "roughly 122,000 amateur rapid matches and almost 700,000 high-rated blitz games. ")])
 
     elif pathname == '/options/option0':
-        with open("rating_distribution.json", "r") as file:
+        # Display content for Option 0: Distributions on Lichess
+        with open("json files/rating_distribution.json", "r") as file:
             rating_distribution = pio.from_json(file.read())
-        with open("games_distribution.json", "r") as file:
+        with open("json files/games_distribution.json", "r") as file:
             games_distribution = pio.from_json(file.read())
 
         return html.Div([
@@ -178,11 +213,12 @@ def update_graph(pathname):
             dcc.Graph(id='option0-chart-2', figure=games_distribution, config=config)])
 
     elif pathname == '/options/option1':
-        with open("number_of_games.json", "r") as file:
+        # Display content for Option 1: Rating Correlation
+        with open("json files/number_of_games.json", "r") as file:
             number_of_games_fig = pio.from_json(file.read())
-        with open("number_of_puzzles.json", "r") as file:
+        with open("json files/number_of_puzzles.json", "r") as file:
             number_of_puzzles = pio.from_json(file.read())
-        with open("puzzle_rating.json", "r") as file:
+        with open("json files/puzzle_rating.json", "r") as file:
             puzzle_rating = pio.from_json(file.read())
 
         return html.Div([
@@ -194,9 +230,10 @@ def update_graph(pathname):
             dcc.Graph(id='option1-chart-3', figure=puzzle_rating, config=config)])
 
     elif pathname == '/options/option2':
-        with open("rating_history_ranks.json", "r") as file:
+        # Display content for Option 2: Rating History
+        with open("json files/rating_history_ranks.json", "r") as file:
             rating_history_ranks = pio.from_json(file.read())
-        with open("rank_distribution.json", "r") as file:
+        with open("json files/rank_distribution.json", "r") as file:
             rank_distribution = pio.from_json(file.read())
 
         return html.Div([
@@ -206,11 +243,12 @@ def update_graph(pathname):
             dcc.Graph(id='option1-chart-2', figure=rank_distribution, config=config)])
 
     elif pathname == '/options/option3':
-        with open("1_a_castling_early_VIOLIN.json", "r") as file:
+        # Display content for Option 3: Castling & Rooks
+        with open("json files/1_a_castling_early_VIOLIN.json", "r") as file:
             fig1_a = pio.from_json(file.read())
-        with open("2_a_rook_act_early_VIOLIN.json", "r") as file:
+        with open("json files/2_a_rook_act_early_VIOLIN.json", "r") as file:
             fig2_a = pio.from_json(file.read())
-        with open("3_a_castle_which_side_BAR.json", "r") as file:
+        with open("json files/3_a_castle_which_side_BAR.json", "r") as file:
             fig3_a = pio.from_json(file.read())
 
         return html.Div([
@@ -222,9 +260,10 @@ def update_graph(pathname):
             dcc.Graph(id='option3-chart-3', figure=fig3_a)])
 
     elif pathname == '/options/option4':
-        with open("4_a_bishop_vs_knight_BAR.json", "r") as file:
+        # Display content for Option 4: Bishops vs Knights
+        with open("json files/4_a_bishop_vs_knight_BAR.json", "r") as file:
             fig4_a = pio.from_json(file.read())
-        with open("4_b_bishop_vs_knight_BAR.json", "r") as file:
+        with open("json files/4_b_bishop_vs_knight_BAR.json", "r") as file:
             fig4_b = pio.from_json(file.read())
 
         return html.Div([
@@ -234,11 +273,12 @@ def update_graph(pathname):
         ])
 
     elif pathname == '/options/option5':
-        with open("5_a_opening_game_mode_MULT_PIE.json", "r") as file:
+        # Display content for Option 5: Openings
+        with open("json files/5_a_opening_game_mode_MULT_PIE.json", "r") as file:
             fig5_a = pio.from_json(file.read())
-        with open("6_a_opening_elo_HEATMAP.json", "r") as file:
+        with open("json files/6_a_opening_elo_HEATMAP.json", "r") as file:
             fig6_a = pio.from_json(file.read())
-        with open("6_b_opening_elo_SCATTER_CAT.json", "r") as file:
+        with open("json files/6_b_opening_elo_SCATTER_CAT.json", "r") as file:
             fig6_b = pio.from_json(file.read())
 
         return html.Div([
@@ -255,9 +295,12 @@ def update_graph(pathname):
         ])
 
     else:
+        # Raise PreventUpdate if pathname doesn't match any options to prevent unintended updates
         raise PreventUpdate
 
 
 if __name__ == '__main__':
+    # Open a new tab in the web browser to access the locally hosted web application
+    # webbrowser.open_new_tab('http://localhost:8050')
+    # Start the Dash app and host it locally on http://localhost:8050 for local development
     app.run_server(debug=True)
-
